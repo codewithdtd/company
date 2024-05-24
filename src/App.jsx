a2b6239077a6e516a733568be3c24e3246c5620d
Dat Do <dtdat201202@gmail.com>
2024-05-24 16:45:13 +0700

    sua lai

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Navigation } from "./components/navigation";
-import { Header } from "./components/header";
-import { Features } from "./components/features";
-import { About } from "./components/about";
-import { Services } from "./components/services";
-import { Gallery } from "./components/gallery";
-import { Testimonials } from "./components/testimonials";
-// import { Team } from "./components/Team";
 import { Contact } from "./components/contact";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
@@ -31,13 +24,6 @@ const App = () => {
   return (
     <ParallaxProvider>
       <Navigation />
-      {/* <Header data={landingPageData.Header} />
-      <Features data={landingPageData.Features} />
-      <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
-      <Gallery data={landingPageData.Gallery} />
-      <Testimonials data={landingPageData.Testimonials} />
-      <Contact data={landingPageData.Contact} /> */}
       <Routes>
         <Route path='/' element={<LangdingPage />} />
         <Route path='/detail/:id' element={<CaseStudiesDetail />}></Route>
